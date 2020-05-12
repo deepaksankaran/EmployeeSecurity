d12ecb538af98b85515045e5b574ca41b038fa7b
Deepak S <sdeepak101@gmail.com>
2020-05-12 10:23:02 +0530

    updated

--- EmployeeSecurity/src/main/java/com/employeesecurity/dao/EmployeeDao.java
+++ EmployeeSecurity/src/main/java/com/employeesecurity/dao/EmployeeDao.java
@@ -1,11 +1,10 @@
 package com.employeesecurity.dao;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.employeesecurity.model.Employee;
 
 @Repository
-public interface EmployeeDao extends CrudRepository<Employee, Long> {
+public interface EmployeeDao extends JpaRepository<Employee, Long> {
     Employee findByUsername(String username);
 }
